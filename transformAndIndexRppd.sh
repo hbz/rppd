431f75132190b36eec6843d301f955dfc9e1e63c
TobiasNx <tobias.buelte@hbz-nrw.de>
2026-07-08 11:41:16 +0200

    Adjust  script for correct Java 11 usage
    Co-authored-by: Fabian Steeg <steeg@hbz-nrw.de>

--- transformAndIndexRppd.sh
+++ transformAndIndexRppd.sh
@@ -5,17 +5,11 @@ TIME=$(date "+%Y%m%d-%H%M")
 INDEX="gnd-rppd-$TIME"
 ALIAS="gnd-rppd-test"
 
-# it is important to set the proper locale
-. $HOME/.locale
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
-JAVA_OPTS=$(echo "$JAVA_OPTS" |sed 's#,#\ #g')
 
 cd ../rpb
 bash transformRppd.sh
-cd -
-sbt clean
-sbt --java-home $JAVA_HOME stage
-JAVA_OPTS="$JAVA_OPTS -XX:+ExitOnOutOfMemoryError" ./target/universal/stage/bin/rppd -Dindex.prod.name=$INDEX "runMain apps.Index baseline"
+sbt --java-home $JAVA_HOME -Dindex.prod.name=$INDEX "runMain apps.Index baseline"
 
 COUNT=$(curl -X POST "indexcluster.lobid.org:9200/$INDEX/_count" | jq .count)
 if (( $COUNT > 10000 )) ; then
